f87cf26135a14fcce8d036d82e46d2b8918a4cc0
Chugar <c.berkane@hotmail.fr>
2025-09-03 22:28:14 +0200

    feat(webview): add svg(r)

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -1,11 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 export default defineConfig({
 	plugins: [
-    react()
-  ],
+		react(),
+		svgr(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
 	build: {
 		outDir: path.resolve(__dirname, "../out/webview"),
 		emptyOutDir: true,
